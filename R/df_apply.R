9ee3391290cef8427b194206922ab30958698644
Adam DenHaan <add22@students.calvin.edu>
2022-03-12 12:44:18 -0500

    change data.frame to tibble in example

--- R/df_apply.R
+++ R/df_apply.R
@@ -10,11 +10,12 @@
 #' @export
 #'
 #' @importFrom purrr map_df
+#' @importFrom dplyr tibble
 #'
 #' @examples
 #' n <- 7L
 #' test_data <-
-#'  data.frame(
+#'  dplyr::tibble(
 #'    double     = rnorm(n, 100, 10),
 #'    integer    = (1L:n) * (1L:n),
 #'    character  = LETTERS[1L:n],
@@ -22,7 +23,7 @@
 #'    logical    = rep(c(TRUE, FALSE), length.out = n)
 #'  )
 #' df_apply(test_data, round, is.numeric, I, digits = 2)
-df_apply <- function(.data, .f, .condition, .else = I, ...) {
+df_apply <- function(.data, .f, .condition, .else = function(x) x, ...) {
   .data |> purrr::map_df(function(x) if (.condition(x)) .f(x, ...) else .else(x))
 }
 
